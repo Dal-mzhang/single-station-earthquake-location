ed10862d7d72875bb5fd0282b063f5b4c2f0f2ad
Miao Zhang <51533859+Dal-mzhang@users.noreply.github.com>
2023-03-16 19:26:32 -0300

    Update runFK.sh

--- simulation/runFK.sh
+++ simulation/runFK.sh
@@ -11,5 +11,6 @@ stk=0
 dip=90
 rak=90
 
+#Trapezoidal source time function -D0.10/0.05, for a complex rupture process; Triangular source time function -D0.10/0.5, for a simple rupture process, which is more popular
 gawk -v stk=$stk -v dip=$dip -v rak=$rak 'BEGIN {for (i=0.01;i<=0.01;i=i+0.1){for (j=7.0;j<=7.4;j=j+0.05){print "syn -M3/"stk"/"dip"/"rak" -D0.10/0.05 -A197.1 -Omodel_EC_"i"/"j".z -Gmodel_EC_"i"/"j".grn.0";}}}' | sh
 gawk -v stk=$stk -v dip=$dip -v rak=$rak 'BEGIN {for (i=0.1;i<=2.1;i=i+0.1){for (j=7.0;j<=7.4;j=j+0.05){print "syn -M3/"stk"/"dip"/"rak" -D0.10/0.05 -A197.1 -Omodel_EC_"i"/"j".z -Gmodel_EC_"i"/"j".grn.0";}}}' | sh
